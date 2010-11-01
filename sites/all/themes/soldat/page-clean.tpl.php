bc2ee8cf516a125f9465a252f57937e54919a338
troelslenda <troels+git@reload.dk>
2010-11-01 22:39:02 +0100

    ticket casetracker

---
+++ sites/all/themes/soldat/page-clean.tpl.php
@@ -0,0 +1,28 @@
+<?php
+// $Id: page.tpl.php,v 1.18.2.1 2009/04/30 00:13:31 goba Exp $
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language ?>" lang="<?php print $language->language ?>" dir="<?php print $language->dir ?>">
+  <head>
+    <?php print $head ?>
+    <title><?php print $head_title ?></title>
+    <?php print $styles ?>
+    <?php print $scripts ?>
+    <!--[if lt IE 7]>
+      <?php print phptemplate_get_ie_styles(); ?>
+    <![endif]-->
+  </head>
+  <body<?php print phptemplate_body_class($left, $right); ?>>
+
+ 
+    <?php if ($tabs): print '<ul class="tabs primary">'. $tabs .'</ul></div>'; endif; ?>
+    <?php if ($show_messages && $messages): print $messages; endif; ?>
+    <?php print $help; ?>
+    <div class="clear-block">
+      <?php print $content ?>
+    </div>
+   
+
+  <?php print $closure ?>
+  </body>
+</html>
